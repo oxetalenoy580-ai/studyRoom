c08e9de9447b3da1305fb6ebedbd851d1ac0d7b6
hhwf <oxetalenoy580@gmail.com>
2026-03-28 15:40:51 +0800

    修改vo为dto

--- study-server/src/main/java/com/study/service/impl/AdminServiceImpl.java
+++ study-server/src/main/java/com/study/service/impl/AdminServiceImpl.java
@@ -3,10 +3,10 @@ package com.study.service.impl;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.study.Result.PageResult;
+import com.study.dto.UserQueryDTO;
 import com.study.entity.User;
 import com.study.mapper.AdminMapper;
 import com.study.service.AdminService;
-import com.study.vo.UserQueryVO;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,9 +17,9 @@ public class AdminServiceImpl implements AdminService {
   @Autowired private AdminMapper adminMapper;
 
   @Override
-  public PageResult getUserList(UserQueryVO queryVO) {
-    PageHelper.startPage(queryVO.getPageNum(), queryVO.getPageSize());
-    List<User> userList = adminMapper.queryUserList(queryVO);
+  public PageResult getUserList(UserQueryDTO queryDTO) {
+    PageHelper.startPage(queryDTO.getPageNum(), queryDTO.getPageSize());
+    List<User> userList = adminMapper.queryUserList(queryDTO);
     PageInfo<User> pageInfo = new PageInfo<>(userList);
     return new PageResult(pageInfo.getTotal(), pageInfo.getList());
   }
